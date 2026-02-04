a49fb7a6c1945eb5b30733f41a4247c5b3e0d7ba
Ubuntu <matheus@clawdbot.kwft3nnk4mnepbnk2clpa5ag4b.bx.internal.cloudapp.net>
2026-02-04 01:14:02 -0300

    chore: set version to 0.1.0-beta

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const program = new Command();
 program
   .name('berean')
   .description('🔍 AI-powered code review for Azure DevOps PRs using GitHub Copilot')
-  .version('1.0.0');
+  .version('0.1.0-beta', '-v, --version', 'Show current version');
 
 program.addCommand(authCommand);
 program.addCommand(reviewCommand);
